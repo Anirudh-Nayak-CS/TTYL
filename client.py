720888d0cfe3d03b6bb4454a7d8f2ac542503f12
Anirudh Nayak <anirudhnayak1508@gmail.com>
2025-07-08 14:59:22 +0530

    working on admin,kick and ban feature

--- client.py
+++ client.py
@@ -7,22 +7,29 @@ FORMAT="utf-8"
 SERVER="127.0.1.1"
 ADDR=(SERVER,PORT)
 DISCONNECT_MESSAGE="/quit"
-
+stop_thread=False
 sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 sock.connect(ADDR)
 def receivemessages():
+ global stop_thread
  while True:
-   msg_length=sock.recv(HEADER).decode(FORMAT)
-   if msg_length:
-    msg_length=int(msg_length)
-    msg=sock.recv(msg_length).decode(FORMAT)
-    print(f"\n{msg}")
+   if stop_thread==True:
+    break
+   rawdata=sock.recv(HEADER)       
+       
+   if not rawdata:
+    break
+ 
+   msg_length=rawdata.decode(FORMAT)
+   msg_length=int(msg_length)
+   msg=sock.recv(msg_length).decode(FORMAT)
+   print(f"\n{msg}")
     
-   else:
-     break
+
 
 def send(msg):
+ if stop_thread==False:
   message=msg.encode(FORMAT)
   msg_length=len(message)
   send_length=str(msg_length).encode(FORMAT)  
@@ -33,21 +40,48 @@ def send(msg):
 
 while True:
 
- username=input("Enter your username -> ")
- user=username.encode(FORMAT)
- user_length=len(user)
- send_user_length=str(user_length).encode(FORMAT)
- send_user_length+=b' '*(HEADER-len(send_user_length))
- sock.send(send_user_length)
- sock.send(user)
- username_existence_msg=sock.recv(1024).decode(FORMAT)
- if "Username already exists" in username_existence_msg:
-    print(username_existence_msg)
-    continue
- else:
-  print(username_existence_msg)
-  break
+  username=input("Enter your username -> ") 
+  username = username.strip() 
+  username=username.replace(' ','_')
+  user=username.encode(FORMAT)
+  user_length=len(user)
+  send_user_length=str(user_length).encode(FORMAT)
+  send_user_length+=b' '*(HEADER-len(send_user_length))
+  sock.send(send_user_length)
+  sock.send(user)
  
+  if username=="admin":
+   password=input("Enter the admin's passsword: ")
+   final_message=password.encode(FORMAT)
+   final_message_length=len(final_message)
+   final_message_length=str(final_message_length).encode(FORMAT)
+   final_message_length+=b' '*(HEADER-len(final_message_length))   
+   sock.send(final_message_length)
+   sock.send(final_message)  
+ 
+   server_pw_valid_msg_length=sock.recv(HEADER).decode(FORMAT)   
+   server_pw_valid_msg_length=int(server_pw_valid_msg_length)
+   server_pw_valid_msg=sock.recv(server_pw_valid_msg_length).decode(FORMAT)
+   
+   if server_pw_valid_msg=="Wrong password":
+     print("Connection refused. Wrong password")
+     stop_thread=True
+     sock.close()
+     exit()
+   else:
+    print(server_pw_valid_msg)
+ 
+  msg_length=sock.recv(HEADER).decode(FORMAT)
+  msg_length=int(msg_length)
+  username_existence_msg=sock.recv(msg_length).decode(FORMAT)
+  
+  if "Username already exists" in username_existence_msg:
+     print(username_existence_msg)
+     continue
+  else:
+   print(username_existence_msg)
+   break
+  
 welcomemsg_length=sock.recv(HEADER).decode(FORMAT)
 welcomemsg_length=int(welcomemsg_length)
 welcomemsg=sock.recv(welcomemsg_length).decode(FORMAT)
@@ -58,6 +92,19 @@ while True:
  message=input()
  if message=="/quit":
    send(DISCONNECT_MESSAGE)
+   stop_thread=True
+   sock.close()
    break
- send(message)
+ elif "/kick" in message:
+   if username=="admin":
+    send(message)
+   else:
+    print("Commands can only be executed by an admin.")
+ elif "/ban" in message:
+  if username=="admin":
+    send(message)
+  else:
+    print("Commands can only be executed by an admin.")
+ else:
+  send(message)
  
--- server.py
+++ server.py
@@ -13,12 +13,16 @@ WELCOME_MESSAGE = (
     "║ /quit               → Disconnect from server   ║\n"
     "║ /msg <username> msg → Privately message a user ║\n"
     "║ /users              → List of users online     ║\n"
+    "║ /kick <username>    → Kick a user              ║\n"
+    "║ /ban <username>     → Ban a user               ║\n"  
     "╚════════════════════════════════════════════════╝"
 
 )
 
 
 clients={}
+admins=[]
+banned_usernames=[]
 
 SERVER=socket.gethostbyname(socket.gethostname())
 ADDR=(SERVER,PORT)
@@ -27,7 +31,7 @@ sock.bind(ADDR)
 
 
 def broadcast(message,currclientusername):
-  final_message=f"[{currclientusername}] {message}".encode(FORMAT)
+  final_message=message.encode(FORMAT)
   final_message_length=len(final_message)
   final_message_length=str(final_message_length).encode(FORMAT)
   final_message_length+=b' '*(HEADER-len(final_message_length))
@@ -37,14 +41,18 @@ def broadcast(message,currclientusername):
        conn.send(final_message_length)
        conn.send(final_message)
 
+
+
 def listallusers(conn):
   online_users=', '.join(clients.keys())
-  final_message=f"[SERVER] Online users -> {online_users}".encode(FORMAT)
+  final_message=f"{online_users}".encode(FORMAT)
   final_message_length=len(final_message)
   final_message_length=str(final_message_length).encode(FORMAT)
   final_message_length+=b' '*(HEADER-len(final_message_length))
   conn.send(final_message_length)
   conn.send(final_message)
+
+
 
 def msgPrivately(message,currclientusername):
   msg_parts=message.split(' ',2)
@@ -52,13 +60,15 @@ def msgPrivately(message,currclientusername):
   connection_sender=clients[currclientusername]
   if len(msg_parts)<3:
     connection_sender.send("[SERVER] Invalid format.Use /msg <username> msg".encode(FORMAT))
+    return
+  
   user=msg_parts[1]
   actualmessage=msg_parts[2]
   if user not in clients:
     connection_sender.send("[SERVER] User is not online".encode(FORMAT))
   else:  
    connection_receiver=clients[user]
-   final_message=f"[ Private message from {currclientusername}] {actualmessage}".encode(FORMAT)
+   final_message=f"[Private message from {currclientusername}] {actualmessage}".encode(FORMAT)
    final_message_length=len(final_message)
    final_message_length=str(final_message_length).encode(FORMAT)
    final_message_length+=b' '*(HEADER-len(final_message_length))
@@ -66,16 +76,56 @@ def msgPrivately(message,currclientusername):
    connection_receiver.send(final_message) 
    
    
-   confirmation_message=f"Your message was sent to {user} successfully"
+   confirmation_message=f"Your message was sent to {user} successfully".encode(FORMAT)
    confirmation_message_length=len(confirmation_message)
    confirmation_message_length=str(confirmation_message_length).encode(FORMAT)
    confirmation_message_length+=b' '*(HEADER-len(confirmation_message_length))  
    connection_sender.send(confirmation_message_length)
-   connection_sender.send(confirmation_message.encode(FORMAT))
+   connection_sender.send(confirmation_message)
 
+def handleKick(msg):
+  msg_parts=msg.split(' ')
+  if len(msg_parts)<2:
+    return
+  username=msg_parts[1]
+  if username!="admin" and username in clients:
+    kick_user_conn=clients[username]
+    del clients[username]
+    message="You were kicked by an admin."
+    final_message=message.encode(FORMAT)
+    final_message_length=len(final_message)
+    final_message_length=str(final_message_length).encode(FORMAT)
+    final_message_length+=b' '*(HEADER-len(final_message_length))   
+    kick_user_conn.send(final_message_length)
+    kick_user_conn.send(final_message)
+    kick_broadcast_msg=f"👢 {username} was kicked from the server."
+    broadcast(kick_broadcast_msg,username)  
+    kick_user_conn.close()
+    return  
+
+def handleBan(msg):
+  msg_parts=msg.split(' ')
+  if len(msg_parts)<2:
+    return
+  username=msg_parts[1]
+  if username!="admin" and username in clients:
+    ban_user_conn=clients[username]
+    banned_usernames.append(username)
+    del clients[username]
+    message="You were banned by an admin."
+    final_message=message.encode(FORMAT)
+    final_message_length=len(final_message)
+    final_message_length=str(final_message_length).encode(FORMAT)
+    final_message_length+=b' '*(HEADER-len(final_message_length))   
+    ban_user_conn.send(final_message_length)
+    ban_user_conn.send(final_message)
+    ban_broadcast_msg=f"🔨 {username} was banned from the server."
+    broadcast(ban_broadcast_msg,username)  
+    ban_user_conn.close()
+    return
 
 def handleClient(conn,addr):
- 
+
   connected=True
   while connected:
     while True:
@@ -84,42 +134,122 @@ def handleClient(conn,addr):
      if username_length:
       username_length=int(username_length)
       username=conn.recv(username_length).decode(FORMAT)
+       
+      if username in banned_usernames:
+        message="You are banned from the server."
+        final_message=message.encode(FORMAT)
+        final_message_length=len(final_message)
+        final_message_length=str(final_message_length).encode(FORMAT)
+        final_message_length+=b' '*(HEADER-len(final_message_length))   
+        conn.send(final_message_length)
+        conn.send(final_message)
+        conn.close()
+        return
 
-      if username in clients:
-       conn.send("[SERVER] Username already exists. Please enter another username.".encode(FORMAT))
+      elif username=="admin": 
+        rawdata=conn.recv(HEADER)       
+       
+        if not rawdata:
+         connected=False
+         if username in clients:
+             del clients[username]
+         if conn in admins:
+            admins.remove(conn)  
+         print(f"[Client {username}] disconnected")
+         broadcast(f" 🔴 {username} left the chat. ",username)
+         break
+        password_length=int(rawdata.decode(FORMAT))
+        password=conn.recv(password_length).decode(FORMAT)
+         
+        if password=="notanadminpassword":
+          message="Welcome Admin!"
+          final_message=message.encode(FORMAT)
+          final_message_length=len(final_message)
+          final_message_length=str(final_message_length).encode(FORMAT)
+          final_message_length+=b' '*(HEADER-len(final_message_length))   
+          conn.send(final_message_length)
+          conn.send(final_message) 
+          clients["admin"] = conn 
+          admins.append(conn)
+          break
+
+        else :
+         message="Wrong password"
+         final_message=message.encode(FORMAT)
+         final_message_length=len(final_message)
+         final_message_length=str(final_message_length).encode(FORMAT)
+         final_message_length+=b' '*(HEADER-len(final_message_length))   
+         conn.send(final_message_length)
+         conn.send(final_message) 
+
+         conn.close()
+         return 
+
+      elif username in clients:
+       message="[SERVER] Username already exists. Please enter another username."
+       final_message=message.encode(FORMAT)
+       final_message_length=len(final_message)
+       final_message_length=str(final_message_length).encode(FORMAT)
+       final_message_length+=b' '*(HEADER-len(final_message_length))   
+       conn.send(final_message_length)
+       conn.send(final_message) 
       
       else:
-       conn.send(f"[SERVER] Username accepted. Welcome! {username}".encode(FORMAT))
+       message=f"[SERVER] Username accepted. Welcome! {username}"
+       final_message=message.encode(FORMAT)
+       final_message_length=len(final_message)
+       final_message_length=str(final_message_length).encode(FORMAT)
+       final_message_length+=b' '*(HEADER-len(final_message_length))   
+       conn.send(final_message_length)
+       conn.send(final_message) 
        clients[username]=conn
        break
+
     print(f"[NEW-CONNECTION] {username} connected")
-    broadcast(f"🟢 {username} joined the chat.",username)
+    if username!="admin":
+     broadcast(f"🟢 {username} joined the chat.",username)
     welcome_msg=WELCOME_MESSAGE.encode('utf8')
     welcome_length=len(welcome_msg)
-    welcome_length=str(welcome_length).encode('utf-8')
+    welcome_length=str(welcome_length).encode(FORMAT)
     welcome_length+=b' '*(HEADER-len(welcome_length))
     conn.send(welcome_length)
     conn.send(welcome_msg)
     while True:
-     msg_length=conn.recv(HEADER).decode(FORMAT)
-     if not msg_length:
-       continue 
-     msg_length=int(msg_length)
-     msg=conn.recv(msg_length).decode(FORMAT)
+      rawdata=conn.recv(HEADER)       
+       
+      if not rawdata:
+         connected=False
+         if username in clients:
+             del clients[username]
+         if conn in admins:
+            admins.remove(conn)  
+         print(f"[Client {username}] disconnected")
+         broadcast(f" 🔴 {username} left the chat. ",username)
+         break
+      msg_length=rawdata.decode(FORMAT)
+      msg_length=int(msg_length)
+      msg=conn.recv(msg_length).decode(FORMAT)
 
-     if msg==DISCONNECT_MESSAGE:
+      if msg==DISCONNECT_MESSAGE:
         connected=False
+        if conn in admins:
+          admins.remove(conn)
         del clients[username] 
         print(f"[CLIENT {username}] disconnected ")
         broadcast(f" 🔴 {username} left the chat. ",username)
         break 
-     print(f"{addr}[{username}]{msg}")
-     if "/msg " in msg:
+      print(f"{addr}[{username}]{msg}")
+      if "/msg " in msg:
        msgPrivately(msg,username)
-     elif "/users" in msg:
+      elif "/users" in msg:
        listallusers(conn) 
-     else:
-      broadcast(msg,username)
+      elif "/ban" in msg and conn in admins:
+        handleBan(msg)
+      elif "/kick" in msg and conn in admins:
+        handleKick(msg)
+      else:
+       msg=f"[{username}] {msg}"
+       broadcast(msg,username)
      
      
   conn.close()
@@ -129,7 +259,7 @@ def start():
   print(f"[LISTENING] Server is listening {SERVER}")
   while True:
     conn,addr=sock.accept()
-    thread=threading.Thread(target=handleClient,args=(conn,addr))
+    thread=threading.Thread(target=handleClient,args=(conn,addr),daemon=True)
     thread.start()
     print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
 
